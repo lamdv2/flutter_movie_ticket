16216e37aca88b0568571193fae21dd76aa058f3
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-04-24 15:44:15 +0700

    update: constant Padding Size

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,14 @@
-class Constants{
-  static final kPadding16 = Margin
-}
+import 'package:flutter/material.dart';
+
+const double kMediumPadding = 24.0;
+const double kBigIconSize = 22.0;
+const double kBottomBarIconSize = 20.0;
+const double kDefaultIconSize = 18.0;
+const double kDefaultPadding = 16.0;
+const double kIconRadius = 14.0;
+const double kItemPadding = 10.0;
+const double kTopPadding = 8.0;
+const double kMinPadding = 4.0;
+
+const BorderRadius kMintBorderRadius = BorderRadius.all(Radius.circular(8.0));
+const BorderRadius kDefaultBorderRadius = BorderRadius.all(Radius.circular(16.0));
