fd12909ae02ce6f8de2877b4b73c83e9e5ca002f
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-04-25 20:42:09 +0700

    add: movie overview

---
+++ lib/common/widgets/stateful/movie_overview.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MovieOverview extends StatefulWidget {
+  const MovieOverview({ Key? key }) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MovieOverviewState createState() => _MovieOverviewState();
+}
+
+class _MovieOverviewState extends State<MovieOverview> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // UI
+    );
+  }
+}
