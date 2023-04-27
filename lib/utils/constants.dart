4bcfceafd739e8b1fb1984b9e03fbfb3d9045009
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-04-27 22:13:26 +0700

    fix: constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
+const double kTop32Padding = 32.0;
 const double kMediumPadding = 24.0;
 const double kBigIconSize = 22.0;
-const double kBottomBarIconSize = 20.0;
-const double kDefaultIconSize = 18.0;
+const double kDefaultIconSize = 20.0;
+const double kLTIconSize = 18.0;
 const double kDefaultPadding = 16.0;
-const double kIconRadius = 14.0;
 const double kItemPadding = 10.0;
 const double kTopPadding = 8.0;
 const double kMinPadding = 4.0;
 
-const BorderRadius kMintBorderRadius = BorderRadius.all(Radius.circular(8.0));
-const BorderRadius kDefaultBorderRadius = BorderRadius.all(Radius.circular(16.0));
+const BorderRadius kMinBorderRadius = BorderRadius.all(Radius.circular(8.0));
+const BorderRadius kMediaBorderRadius = BorderRadius.all(Radius.circular(10.0));
+const BorderRadius kDefaultBorderRadius = BorderRadius.all(Radius.circular(14.00));
+const BorderRadius kmaxBorderRadius = BorderRadius.all(Radius.circular(16.0));
