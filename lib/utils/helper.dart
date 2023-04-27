af917f08de2d7bcc24ed1953e03fa8d6f68879d8
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-04-27 22:13:47 +0700

    update: helper img, ico

--- lib/utils/helper.dart
+++ lib/utils/helper.dart
@@ -1,12 +1,38 @@
 mixin AssetHelper {
-  //Assets in icons
-  static const String icoOvalTop = 'assets/icons/ico_oval_top.png';
-  static const String icoOvalBottom = 'assets/icons/ico_oval_bottom.png';
-  static const String icoHotel = 'assets/icons/ico_hotel.png';
-  static const String icoPlane = 'assets/icons/ico_plane.png';
-  static const String icoHotelPlane = 'assets/icons/ico_hotel_plane.png';
+  // Assets in icons
+  static const String icoAdd = 'assets/icons/add.png';
+  static const String icoBack = 'assets/icons/back.png';
+  static const String icoCancel = 'assets/icons/cancel.png';
+  static const String icoControl = 'assets/icons/control.png';
+  static const String icoHomeActive = 'assets/icons/home_active.png';
+  static const String icoHomeInactive = 'assets/icons/home_inactive.png';
+  static const String icoLocation = 'assets/icons/location.png';
+  static const String icoNext = 'assets/icons/next.png';
+  static const String icoPlay = 'assets/icons/play.png';
+  static const String icoPocketActive = 'assets/icons/pocket_active.png';
+  static const String icoPocketInactive = 'assets/icons/pocket_inactive.png';
+  static const String icoSearch = 'assets/icons/search.png';
+  static const String icoTicketActive = 'assets/icons/ticket_active.png';
+  static const String icoTicketInactive = 'assets/icons/ticket_inactive.png';
+  static const String icoTopUp = 'assets/icons/top_up.png';
 
-  //Assets in images
-  static const String backgroundSplash = 'assets/images/background_splash.png';
-  static const String circleSplash = 'assets/images/circle_splash.png';
+  // Assets in images
+  static const String imgApple = 'assets/images/apple.png';
+  static const String imgGoogle = 'assets/images/google.png';
+  static const String imgProfile = 'assets/images/profile.png';
+
+  // Assets in images x2
+  static const String imgBarCode = 'assets/images/x2/barcode.png';
+  static const String imgDragon = 'assets/images/x2/dragon.png';
+  static const String imgFrozen = 'assets/images/x2/frozen.png';
+  static const String imgOnward = 'assets/images/x2/onward.png';
+  static const String imgRalph = 'assets/images/x2/ralph.png';
+  static const String imgScoob = 'assets/images/x2/scoob.png';
+  static const String imgSpongebob = 'assets/images/x2/spongebob.png';
+  static const String imgTopUp = 'assets/images/x2/top_up.png';
+
+  // Assets in images x3
+  static const String imgTrailer1 = 'assets/images/x3/trailer1.png';
+  static const String imgTrailer2 = 'assets/images/x3/trailer2.png';
+
 }
