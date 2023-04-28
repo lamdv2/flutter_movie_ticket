7def03b98759d6dc24615c0797a4f8d6e3aee07c
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-04-28 22:54:48 +0700

    add: profile

---
+++ lib/screens/profile/profile_screen.dart
@@ -0,0 +1,58 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:movie_ticket/themes/app_colors.dart';
+import 'package:movie_ticket/themes/app_styles.dart';
+import 'package:movie_ticket/utils/constants.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  static const String routeName = '/profile_screen';
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+void signUserOut() {
+  FirebaseAuth.instance.signOut();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final userName = ModalRoute.of(context)!.settings.arguments;
+    const a = 'f';
+    return Scaffold(
+      appBar: AppBar(
+        actions: const [
+          IconButton(
+            onPressed: signUserOut,
+            icon: Icon(Icons.logout),
+          ),
+        ],
+        backgroundColor: AppColors.darkBackground,
+        centerTitle: true,
+        title: Text(
+          'Your Profile',
+          style: AppStyles.h2,
+        ),
+        automaticallyImplyLeading: true,
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(kDefaultPadding),
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text(userName != null ? 'Hi ${userName.toString()}!' : 'Hi User!',
+                  style: AppStyles.h3,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
