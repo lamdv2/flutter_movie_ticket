dadba9b3babc82ce01f3b1272b2147daa8839d87
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-05-02 23:24:49 +0700

    fix: carousel slide finish

--- lib/screens/home/widgets/carousel_slide.dart
+++ lib/screens/home/widgets/carousel_slide.dart
@@ -1,22 +1,23 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_ticket/common/widgets/stateless/star_half_widget.dart';
 import 'package:movie_ticket/screens/home/widgets/title.dart';
 import 'package:movie_ticket/themes/app_colors.dart';
 import 'package:movie_ticket/themes/app_styles.dart';
-import 'package:movie_ticket/utils/helper.dart';
 
+import '../../../common/widgets/stateless/star_widget.dart';
 import '../../../models/movie.dart';
 import '../../../utils/constants.dart';
 
 class CarouselSliderFirm extends StatefulWidget {
   const CarouselSliderFirm({
     super.key,
-    required this.genres,
+    required this.movies,
     required this.size,
   });
 
   final Size size;
-  final List<Movie> genres;
+  final List<Movie> movies;
 
   @override
   State<CarouselSliderFirm> createState() => _CarouselSliderFirmState();
@@ -39,25 +40,80 @@ class _CarouselSliderFirmState extends State<CarouselSliderFirm> {
               enlargeCenterPage: true,
               viewportFraction: 0.6,
             ),
-            items: listMovies.map((i) {
+            items: widget.movies.map((i) {
               return Builder(
                 builder: (BuildContext context) {
-                  return Container(
-                    width: widget.size.width,
-                    margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                    decoration: BoxDecoration(
-                      color: AppColors.lightBlueNon,
-                      borderRadius: kBigBorderRadius,
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage(i.background.toString()),
+                  return Stack(
+                    children: [
+                      Container(
+                        width: widget.size.width,
+                        height: widget.size.height,
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        decoration: BoxDecoration(
+                          color: AppColors.lightBlueNon,
+                          borderRadius: kBigBorderRadius,
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage(i.background.toString()),
+                          ),
+                        ),
                       ),
-                    ),
+                      Container(
+                        height: widget.size.height,
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Gradients.darkGreyNon,
+                              Gradients.darkGreyMid,
+                              Gradients.darkGrey,
+                            ],
+                          ),
+                          borderRadius: kBigBorderRadius,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              width: widget.size.width,
+                              padding: const EdgeInsets.only(
+                                left: kTopPadding,
+                                bottom: kTopPadding,
+                              ),
+                              child: Text(
+                                i.name.toString(),
+                                style: AppStyles.h1,
+                              ),
+                            ),
+                            Container(
+                              width: widget.size.width,
+                              padding: const EdgeInsets.only(
+                                left: kTopPadding,
+                                bottom: kTopPadding,
+                              ),
+                              margin: const EdgeInsets.only(bottom: kDefaultPadding),
+                              child: Row(
+                                children: [
+                                  if (i.star != null && i.star! >= 4)
+                                    ...List.generate(4, (index) =>  const StarWidget()),
+                                  if (i.star != null && i.star! > 4 && i.star! < 5)
+                                    const StarHalfWidget(),
+                                  if (i.star != null && i.star! == 5)
+                                    ...List.generate(5, (index) => const StarWidget()),
+                                  if (i.star != null && i.star! < 4)
+                                    ...List.generate(3, (index) => const StarWidget()),
+                                  if (i.star != null && i.star! > 3 && i.star! < 4)
+                                    const StarHalfWidget(),
+                                  Text(' (${i.star.toString()})', style: AppStyles.h5,),
+                                ],
+                              ),
 
-                    child: Text(
-                      '${i.getType()}',
-                      style: const TextStyle(fontSize: 32.00),
-                    ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   );
                 },
               );
@@ -68,3 +124,5 @@ class _CarouselSliderFirmState extends State<CarouselSliderFirm> {
     );
   }
 }
+
+
